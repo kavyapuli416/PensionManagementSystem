6e169227354f1b229ab7a34d328d5e3faeeebfbb
Kavyapuli <pulikavya10@gmail.com>
2026-06-18 21:03:02 +0530

    Day 1 - Database setup and EF Core configuration

---
+++ PensionManagement.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PensionManagement.API.Models;
+
+namespace PensionManagement.API.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+            {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ PensionManagement.API/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace PensionManagement.API.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeCode { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public DateTime JoiningDate { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
+
+        public string Designation { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
