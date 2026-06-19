c9dc44edcf1dc1b91aa9d7d7552dc9c151da7011
Kavyapuli <pulikavya10@gmail.com>
2026-06-19 19:05:23 +0530

    Day 2 - Employee GET API implemented

---
+++ PensionManagement.API/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PensionManagement.API.Data;
+
+namespace PensionManagement.API.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+          var employees=_context.Employees.ToList();
+           return Ok(employees);
+        }
+    }
+}
