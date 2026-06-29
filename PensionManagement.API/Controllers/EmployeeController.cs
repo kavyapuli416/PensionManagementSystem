ca98908aba7c77dcba3a59719a4d1b050c8994e9
Kavyapuli <pulikavya10@gmail.com>
2026-06-29 19:20:39 +0530

    Day 3 - Completed Employee CRUD APIs

--- PensionManagement.API/Controllers/EmployeeController.cs
+++ PensionManagement.API/Controllers/EmployeeController.cs
@@ -28,5 +28,57 @@ namespace PensionManagement.API.Controllers
             _context.SaveChanges();
             return Ok(employee);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound("Employee Not Found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id,Employee updatedEmployee)
+        {
+            var employee= _context.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound("Employee Not Found");
+            }
+            employee.EmployeeCode = updatedEmployee.EmployeeCode;
+            employee.EmployeeName = updatedEmployee.EmployeeName;
+            employee.Email = updatedEmployee.Email;
+            employee.PhoneNumber = updatedEmployee.PhoneNumber;
+            employee.DateOfBirth = updatedEmployee.DateOfBirth;
+            employee.JoiningDate = updatedEmployee.JoiningDate;
+            employee.Salary = updatedEmployee.Salary;
+            employee.Department = updatedEmployee.Department;
+            employee.Designation = updatedEmployee.Designation;
+            employee.IsActive = updatedEmployee.IsActive;
+
+            _context.SaveChanges();
+            return Ok(employee);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound("Employee not found.");
+            }
+
+            _context.Employees.Remove(employee);
+
+            _context.SaveChanges();
+
+            return Ok("Employee deleted successfully.");
+        }
     }
 }
